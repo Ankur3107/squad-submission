bb013a7d0d7cf8c1fad7d55710e73037048a335d
Ankur3107 <ankur310794@gmail.com>
2020-02-12 16:30:09 +0530

    final submission

--- script.sh
+++ script.sh
@@ -2,4 +2,12 @@ export PYTHONPATH=${PYTHONPATH}:`pwd`; mkdir full_code; cp model/ctl_step_6992.c
 
 mkdir full_code; cp model/ctl_step_6992.ckpt-3.data-00001-of-00002 full_code/; cp -r code/ full_code/; cd full_code
 
-cl run 	dev-v2.0.json:dev-v2.0.json :code :model "export PYTHONPATH=${PYTHONPATH}:`pwd`; mkdir full_code; cp -r code/ full_code/; cp model/ctl_step_6992.ckpt-3.data-00001-of-00002 full_code/code/; cp dev-v2.0.json full_code/code/; cd full_code/code; python predict_squad.py --predict_file dev-v2.0.json" -n run-predictions-3 --request-docker-image mingdachengoogle/tf_sp_gpu:latest --request-memory 8g --request-cpus 2 --request-gpus 1
+cl run 	dev-v2.0.json:dev-v2.0.json :code :model "mkdir full_code; cp -r code/ full_code/; cp model/ctl_step_6992.ckpt-3.data-00001-of-00002 full_code/code/; cp dev-v2.0.json full_code/code/; cd full_code/code; python predict_squad.py --predict_file dev-v2.0.json" -n run-predictions-3 --request-docker-image ankur310794/tensorflow:tfv2.0 --request-memory 8g --request-cpus 2 --request-gpus 1
+
+cl run 	dev-v2.0.json:dev-v2.0.json :code :model "mkdir full_code; cp -r code/ full_code/; cp model/ctl_step_6992.ckpt-3.data-00001-of-00002 full_code/code/; cp dev-v2.0.json full_code/code/; cd full_code/code; python predict_squad.py --predict_file dev-v2.0.json --model_dir ../../" -n run-predictions-dev --request-docker-image ankur310794/tensorflow:tfv2.0 --request-memory 8g --request-cpus 2 --request-gpus 1
+
+cl run 	dev-v2.0.json:dev-v2.0.json :code :model "mkdir full_code; cp -r code/ full_code/; cp model/ctl_step_6992.ckpt-3.data-00001-of-00002 full_code/code/; cp dev-v2.0.json full_code/code/; cd full_code/code; python predict_squad.py --predict_file dev-v2.0.json --model_dir ../../ --strategy_type mirror --predict_batch_size 16" -n run-predictions-dev --request-docker-image ankur310794/tensorflow:tfv2.0 --request-memory 10g --request-cpus 2 --request-gpus 2
+
+cl run 	dev-vs2.0.json:dev-vs2.0.json :code :model "mkdir full_code; cp -r code/ full_code/; cp model/ctl_step_6992.ckpt-3.data-00001-of-00002 full_code/code/; cp dev-vs2.0.json full_code/code/; cd full_code/code; python predict_squad.py --predict_file dev-vs2.0.json" -n run-predictions-3 --request-docker-image ankur310794/tensorflow:latest --request-memory 8g --request-cpus 2 --request-gpus 1
+
+cl run 	dev-vs2.0.json:dev-vs2.0.json :code :model "mkdir full_code; cp -r code/ full_code/; cp model/ctl_step_6992.ckpt-3.data-00001-of-00002 full_code/code/; cp dev-vs2.0.json full_code/code/; cd full_code/code; python predict_squad.py --predict_file dev-vs2.0.json --model_dir ../../" -n run-predictions-3 --request-docker-image ankur310794/tensorflow:latest --request-memory 8g --request-cpus 2 --request-gpus 1
